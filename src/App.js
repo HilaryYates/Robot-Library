c53856f22bffc2cdea112f28dc9f54c9c2516321
hilaryyates <hilryyates@gmail.com>
2022-09-11 15:48:49 -0400

    func Class app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import { robots } from "./robots.js";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
+// import { Component } from "react";
+import { render } from "@testing-library/react";
 
-const App = () => {
-  return (
-    <div className='tc'>
-      <h1>Robots</h1>
-      <SearchBox />
-      <CardList props={robots} />
-    </div>
-  );
-};
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { robots, searchField: "" };
+  }
+  render() {
+    return (
+      <div className='tc'>
+        <h1>Robots</h1>
+        <SearchBox />
+        <CardList props={robots} />
+      </div>
+    );
+  }
+}
 
 export default App;
