f5f80b80940f482acaef6dcb3800ea7da1a51b85
hilaryyates <hilryyates@gmail.com>
2022-09-19 18:12:31 -0400

    files seperated by components and containers

---
+++ src/containers/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+// import { robots } from "./robots.js";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
+// import "./index.css";
+import { render } from "@testing-library/react";
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { robots: [], searchField: "" };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) => {
+      return robots.name
+        .toLowerCase()
+        .includes(this.state.searchField.toLowerCase());
+    });
+    if (this.state.robots.length === 0) {
+      return <h1>Loading..</h1>;
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f2'>Robots</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <Scroll>
+            <CardList props={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+    }
+  }
+}
+
+export default App;
