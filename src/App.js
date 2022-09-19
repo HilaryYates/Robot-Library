2b4c32431ae4ec433dd51af28610a9f777be6b27
hilaryyates <hilryyates@gmail.com>
2022-09-19 17:09:46 -0400

    func loading

--- src/App.js
+++ src/App.js
@@ -11,9 +11,10 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => this.setState({ robots: users }));
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json()
+    );
+    .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -26,13 +27,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchField.toLowerCase());
     });
-    return (
-      <div className='tc'>
-        <h1 className='f2'>Robots</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList props={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h1>Loading..</h1>;
+    } else {
+      return (
+        <div className='tc'>
+          <h1 className='f2'>Robots</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList props={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
