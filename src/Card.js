6454ccaf5ee7008983aa5cda043de85c46004382
hilaryyates <hilryyates@gmail.com>
2022-09-19 17:01:58 -0400

    style robot header txt font size

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = ({ id, name, email }) => {
   return (
     <div className='ts dib br3 pa3 ma2 grow bw2 shadow-5  '>
-      <h1>Robot</h1>
+      <h1 className='f5'>Robot</h1>
       <img alt='robots' src={`https://robohash.org/${id} ?size=200x200`} />
       <div>
         <h2>{name}</h2>
