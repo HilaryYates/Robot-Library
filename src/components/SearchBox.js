b66949ff3148a0de516d93e2dee32d769821885d
hilaryyates <hilryyates@gmail.com>
2022-11-12 19:49:36 -0500

    styles

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,13 +4,14 @@ const SearchBox = ({ searchField, searchChange }) => {
   return (
     <div className='pa2'>
       <h1 className='title'>Search for the name of the Robot.</h1>
-
-      <input
-        className='pa3 ba search-box'
-        type='search'
-        placeholder='search robots'
-        onChange={searchChange}
-      ></input>
+      <div className='search-box-container'>
+        <input
+          className='pa3 ba'
+          type='search'
+          placeholder='search robots'
+          onChange={searchChange}
+        ></input>
+      </div>
     </div>
   );
 };
